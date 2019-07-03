9a3c0d7ad8a44b06498fc8cd5dbe398d4bcfe5a8
Alba Cruz <albacruztorres@gmail.com>
2019-07-03 16:35:07 +0100

    Changes in package.json

---
+++ src/api/router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const usuarios = require("../database/usuarios");
+
+router.get("/", (req, res) => {
+  res.json(usuarios);
+});
+
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = usuarios.find(user => user.id == userId);
+  /* console.log("Request: ", req.headers); */
+  return res.json(user);
+});
+
+module.exports = router;
